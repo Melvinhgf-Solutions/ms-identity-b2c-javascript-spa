9dd1adba714bd77143fa3474de27f5dd989da613
Muhammad Faizullah <b.anthracis@live.com>
2021-08-15 20:56:45 +0800

    Update apiConfig.js

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,6 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
-    b2cScopes: ["https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read"],
-    webApi: "https://fabrikamb2chello.azurewebsites.net/hello"
+    b2cScopes: ["https://melvinhgfnetb2c.onmicrosoft.com/tasks-api/tasks.write"],
+    b2cScopes: ["https://melvinhgfnetb2c.onmicrosoft.com/tasks-api/tasks.read"],
+    webApi: "https://melvinhgfnetb2c.onmicrosoft.com/tasks-api"
   };
